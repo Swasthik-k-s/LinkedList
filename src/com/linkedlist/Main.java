b15df2b43df273566e46d3c9db004381b9b84a4e
Swasthik <swasthik.ks22@gmail.com>
2021-09-12 19:24:25 +0530

    Ability to add at specified Position

--- src/com/linkedlist/Main.java
+++ src/com/linkedlist/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		LinkedList linkedList = new LinkedList();
 		linkedList.addEnd(56);
-		linkedList.addEnd(30);
 		linkedList.addEnd(70);
+		linkedList.addMiddle(30, 2);
 		linkedList.print();
 	}
 }
