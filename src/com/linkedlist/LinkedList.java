5bec01f12955c6bc3ce511712b2501a778a1a19c
Swasthik <swasthik.ks22@gmail.com>
2021-09-12 15:30:40 +0530

    Created a simple Linked List

---
+++ src/com/linkedlist/LinkedList.java
@@ -0,0 +1,43 @@
+package com.linkedlist;
+
+public class LinkedList {
+	
+	private Node head;
+	
+	private class Node {
+		
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	public void addEnd(int data) {
+		Node newNode = new Node(data);
+		
+		if(head == null) {
+			head = newNode;
+		} else {
+			Node temp = head;
+			
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+		}
+	}
+	
+	public void print() {
+		Node temp = head;
+		System.out.println("Linked List");
+		
+		while(temp.next != null) {
+			System.out.print(temp.data + "->");
+			temp = temp.next;
+		}
+		System.out.println(temp.data);
+	}
+}
---
+++ src/com/linkedlist/Main.java
@@ -0,0 +1,13 @@
+package com.linkedlist;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+		linkedList.addEnd(56);
+		linkedList.addEnd(30);
+		linkedList.addEnd(70);
+		linkedList.print();
+	}
+}
+
